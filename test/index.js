fc277432a484a13a1152f25f84e1c977c0c5e36d
laoshu133 <admin@laoshu133.com>
2016-06-28 01:38:30 +0800

    Fix test bug

--- test/index.js
+++ test/index.js
@@ -1,6 +1,24 @@
 'use strict';
 
-describe('Git deployer', function() {
-  require('./deployer');
-  require('./parse_config');
+require('chai').should();
+
+describe('uri filter', function() {
+    var ctx = {
+        log: {
+            info: function() {}
+        }
+    };
+
+    var filter = require('../lib/filter').bind(ctx);
+
+    it('default', function() {
+        var content = '![test image](../assets/wrt/p3.jpg)';
+        var result = '![test image](/assets/wrt/p3.jpg)';
+
+        var data = filter({
+            content: content
+        });
+
+        data.content.should.eql(result);
+    });
 });
